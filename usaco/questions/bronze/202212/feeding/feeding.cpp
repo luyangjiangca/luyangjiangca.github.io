bb6ed73cfd663a385fbb0e27b04bcb103d98424e
juwang <juwang@paloaltonetworks.com>
2024-07-06 21:04:01 -0700

    usaco 202212 bronze feeding and reverse question added

---
+++ usaco/questions/bronze/202212/feeding/feeding.cpp
@@ -0,0 +1,19 @@
+//https://usaco.org/index.php?page=viewproblem2&cpid=1252
+/*
+ID: luyangj1
+PROG: feeding
+LANG: C++
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main(){
+	// ofstream fout ("feeding.out");
+  // ifstream cin ("1.in");
+	int N;
+  cin >> N;
+}
---
+++ usaco/questions/bronze/202212/reverse/reverse.cpp
@@ -0,0 +1,19 @@
+//https://usaco.org/index.php?page=viewproblem2&cpid=1253
+/*
+ID: luyangj1
+PROG: reverse
+LANG: C++
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main(){
+	// ofstream fout ("feeding.out");
+  // ifstream cin ("1.in");
+	int N;
+  cin >> N;
+}
