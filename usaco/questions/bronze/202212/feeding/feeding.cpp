22148fef57e577c33eb03eee5bf331dea23be749
l j <luyangjiangca@gmail.com>
2024-07-14 19:55:11 -0700

    7/14/2024 2012 winter problems done

--- usaco/questions/bronze/202212/feeding/feeding.cpp
+++ usaco/questions/bronze/202212/feeding/feeding.cpp
@@ -4,16 +4,77 @@ ID: luyangj1
 PROG: feeding
 LANG: C++
 */
-#include <iostream>
 #include <fstream>
 #include <string>
 #include <algorithm>
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 int main(){
-	// ofstream fout ("feeding.out");
-  // ifstream cin ("1.in");
-	int N;
-  cin >> N;
+
+  int cases;
+  cin >> cases;
+  for(int i = 0; i<cases; i++)
+  {
+    int grass = 0;
+    int numcows;
+    cin >> numcows;
+    int movedist;
+    cin >>movedist;
+    string a;
+    string cows="";
+    string feed="";
+    for(int i = 0; i<numcows;i++)
+    {
+      feed.append(".");
+    }
+    cin >> cows;
+    int prevG = movedist+movedist+1;
+      int prevH = movedist+movedist+1;
+    int T = numcows;
+    int x = -1;
+
+    while(T--){
+      x++;
+      int fed = 0;
+      char currentCow = cows[x];
+      bool feeded = false;
+      if(prevG<=movedist&&currentCow=='G')
+      {
+          feeded = true;
+      }else if(prevH<=movedist&&currentCow=='H')
+      {
+        feeded = true;
+      }
+      if(!feeded)
+      {
+        for(int i = movedist; i>=-movedist; i--)
+        {
+            if('.'==feed[x+i])
+            {
+              feed[x+i] = currentCow;
+              grass++;
+              if(currentCow=='G')
+              {
+                prevG = -i+1;
+                prevH++;
+              }else{
+                prevH = -i+1;
+                prevG++;
+              }
+              break;
+            }
+            
+        }
+      }else{
+          prevG++;
+          prevH++;
+        }
+    }
+    cout<<grass<<endl;
+    cout<<feed;
+    cout<<endl;
+  }
+  // cin.get();//windows need this line to show the output in terminal console, commnent out when submit
 }
--- usaco/questions/bronze/202212/reverse/reverse.cpp
+++ usaco/questions/bronze/202212/reverse/reverse.cpp
@@ -8,12 +8,101 @@ LANG: C++
 #include <fstream>
 #include <string>
 #include <algorithm>
-#include <iostream>
+// #include <iostream>
 using namespace std;
+int rows,cols;
+string inputs[100];
+int outputs[100];
+bool splits[100];
 
+
+bool split (int j, int compare, int output)
+{
+  if(splits[j])
+  {
+    return false;
+  }
+  for(int y = 0; y<cols; y++)
+  {
+    if(!inputs[y].empty() && (inputs[y][j]==compare) && (outputs[y]!=output))
+    {
+      return false;
+    }
+  }
+  splits[j] = true;
+  for(int w = 0; w<cols;w++)
+  {
+    if(!inputs[w].empty() && inputs[w][j] == compare)
+    {
+      inputs[w] = "";
+    }
+  }
+  return true;
+}
+
+bool all_equal(int test_out)
+{
+  for(int i = 0; i<cols; i++)
+  {
+    if(!inputs[i].empty()&&(outputs[i]!= test_out))
+    {
+      return false;
+    }
+  }
+  return true; 
+}
 int main(){
-	// ofstream fout ("feeding.out");
-  // ifstream cin ("1.in");
-	int N;
-  cin >> N;
+  // ifstream fin ("C:\\Users\\luyan_\\luyangjiangca.github.io\\usaco\\questions\\bronze\\202212\\reverse\\1.in");
+  // cout <<"sussyuwubakaobby";
+  int T;
+  cin>>T;
+  // cout << T;
+  // cout << "696";
+  while(T--)
+  {
+    // ifstream fin ("1.in");
+    
+    cin>>rows; //N
+    cin>>cols;//M
+    for(int i = 0; i<cols; i++)
+    {
+      cin>>inputs[i]>>outputs[i];
+    }
+    for(int ii = 0; ii<rows; ii++)
+    {
+      splits[ii] = false;
+    }
+    // for(int col = 0; col<cols; col++)
+    // {
+    //   for(int row = 0; row<rows; row++)
+    //   {
+
+    //   }
+    // }
+    while (true)
+    {
+       bool followrule = false;
+
+       for(int x = 0; x<rows; x++)
+       {
+        if(split(x, '0', 0)||
+        split(x, '1', 0)||split(x, '0', 1)||
+        split(x, '1', 1)){
+          followrule = true;
+        }
+       }
+       if(!followrule)
+       {
+         cout << "LIE"<<endl;
+         break;
+       }
+       if(all_equal(0)||all_equal(1)){
+        cout<<"OK"<<endl;
+        break;
+       }
+    }
+    
+  }
+  // cout<<65677;
+  // cin.get();
 }
